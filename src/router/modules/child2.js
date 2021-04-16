531a6eac2fa6ae7f7c39583f17474259b181e113
zhengllingling <1072828530@qq.com>
2021-04-16 15:42:47 +0800

    feat 路由懒加载

--- src/router/modules/child2.js
+++ src/router/modules/child2.js
@@ -1,11 +1,13 @@
-import Child2 from "../../views/home/child2";
+// import Child2 from "../../views/home/child2";
 
 const child2 = {
     path: 'child2',
     name: 'child2',
     // // 别名
     // alias: 'child3',
-    component: Child2,
+    // component: Child2,
+    // 路由懒加载
+    component: () => import("../../views/home/child2"),
     beforeEnter: (to, from, next) => {
         console.log(to, 'child2 beforeEnter')
         next();
